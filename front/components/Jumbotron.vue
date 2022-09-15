06425a6c4f7de9e2371ceb8af13fee5f6004e59d
Julio Morales <ja.morales11@uniandes.edu.co>
2022-09-15 07:00:55 -0500

    Detalles back

--- front/components/Jumbotron.vue
+++ front/components/Jumbotron.vue
@@ -2,7 +2,9 @@
     <div id="jumbo">
         <svg width="900" height="100">
             <rect width="900" height="100" rx="20"  fill="#FFF7EB"/>
+            
         </svg>
+        <img src="../assets/images/headerimage.png" width="100" height="100" />
     </div>
 </template>
 
